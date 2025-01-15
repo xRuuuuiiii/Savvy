23146efb330c66995bc83a7102296958b7790c57
xRuuuuiiii <155722219+xRuuuuiiii@users.noreply.github.com>
2025-01-15 09:31:17 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "Savvy",
+    introTitle: 'Savvanah',
+    introDesc: `Can’t stop thinking about you—wish I could hear your voice every minute.`,
+    btnIntro: 'Click this',
+    title: 'What should I say?',
+    desc: 'I think I was just completely taken by your smile and your voice—it really made my heart skip a beat. Do you feel the same way?',
+    btnYes: 'I want to know more about you!<3',
+    btnNo: 'No :3',
+    question: 'There’s something really special about you, and I’d love to spend more time with you. Would you be open to letting me court you?<3',
+    btnReply: 'G? click mo pa to <3',
+    reply: 'no choice ka po YES lang agad sagot hahaha :)',
+    mess: 'I knew it!!🤣',
+    messDesc: 'Pick you up to go out soon, okay? ',
+    btnAccept: 'Message me here pero your choice ok lang kung hindi🤣<3',
+    messLink: 'https://www.instagram.com/renzznj/'
+}
